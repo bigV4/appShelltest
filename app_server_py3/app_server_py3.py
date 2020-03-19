d2b410496a77ab09042e0dccc88c1afd59647139
liudyixia <liudyixia@liudyixiade.local>
2020-03-19 21:34:39 +0800

    update

--- app_server_py3/app_server_py3.py
+++ app_server_py3/app_server_py3.py
@@ -177,7 +177,7 @@ def init_db():
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),'img/favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/index/")
